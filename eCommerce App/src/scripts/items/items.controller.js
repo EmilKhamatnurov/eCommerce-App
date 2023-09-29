4d715257e15bf24d8a228a4a680fe0ce319d5e86
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2023-09-29 18:19:30 +0500

    Добавил что-то

--- eCommerce App/src/scripts/items/items.controller.js
+++ eCommerce App/src/scripts/items/items.controller.js
@@ -1,10 +1,12 @@
 import ItemsModel from "./items.model";
 import ItemsView from "./items.view";
 import Firebase from "../firebase/firebase";
+import LocalStorage from "./localStorage";
 
 class ItemsController {
 	constructor() {
 		this.firebase = new Firebase();
+		this.localStorage = new LocalStorage();
 		this.model = new ItemsModel();
 		this.view = new ItemsView();
 	}
@@ -12,6 +14,7 @@ class ItemsController {
 	init() {
 		this.firebase.readItems() 
 			.then(itemsData => {
+				this.localStorage.saveToLocalStorage();
 				this.model.saveItems(itemsData);
 				this.view.renderItemsList(this.model.items);
 			})
---
+++ eCommerce App/src/scripts/items/localStorage.js
@@ -0,0 +1,10 @@
+class LocalStorage {
+	constructor() {
+		this.localStorage = localStorage.setItem('cart', 'Пустая');
+	}
+	saveToLocalStorage = () => {
+		
+	}
+}
+
+export default LocalStorage
